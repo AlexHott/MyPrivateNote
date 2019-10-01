82cbea89512446ba4cdba77b0b8051eae8748695
AlexHott <AlexHott@yandex.ru>
2019-10-01 21:52:22 +0500

    добавил тестовый проект

--- TestMyPrivateNote/UnitTest1.cs
+++ TestMyPrivateNote/UnitTest1.cs
@@ -7,34 +7,5 @@ namespace TestMyPrivateNote
 {
     public class UnitTestPrivateNote
     {
-        private readonly MyPrivateNotes _myPrivateNote;
-
-        public UnitTestPrivateNote()
-        {
-            _myPrivateNote = new MyPrivateNotes();
-        }
-
-        [Fact]
-        public void CorrectData()
-        {
-
-            Assert.Equal(0, 0);
-        }
-
-        []
-        [Theory]
-        [InlineData(new List<MyNoteData>(),
-                    new MyNoteData { ID=0, TITLE = "Первый тест", BODY = "Содержимое сообщения", IsDeleted = false },
-                    new MyNoteResult { Result = true, Message = "Note sucessfuly added", NoteData = new List<MyNoteData>() }
-                    )]
-       
-        public void MyPrivateNoteCorrectOperations(List<MyNoteData> myNoteDataList, MyNoteData myNoteData, MyNoteResult result)
-        {
-
-            var testResult = _myPrivateNote.AddNote(myNoteDataList, myNoteData);
-
-            Assert.Equal(result.Result, testResult.Result);
-            Assert.Equal(result.Message, testResult.Message);
-        }
-    }
+    }    
 }
